782e56b2a49d2d6596c36af9c5369fcac00699ac
Dmitriy Alexandrov <newsmobileads@gmail.com>
2024-12-06 23:11:19 +0500

    Первая версия программы (по документации)

---
+++ main.py
@@ -0,0 +1,47 @@
+import requests
+import argparse
+
+# gpt://<идентификатор_каталога>/yandexgpt-lite/deprecated
+URL = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+IAM_TOKEN = "t1.9euelZrNm4zGicqZiszJl8fGipWXj-3rnpWakZiLlouOyJ2OlJeLmJqex5Hl8_dYNTRF-e9QOWBj_N3z9xhkMUX571A5YGP8zef1656Vmsmam42WyJ2OnZeUmp2TyZaa7_zF656Vmsmam42WyJ2OnZeUmp2TyZaa.EpCqJ3VwxqFxw9P0IEEzRUXhgLSXvlEIRlX58XusJtorAxiTrYjoby_jMjmskNAkO-WcesIhGzesVaEmb5aQBw"
+FOLDER_ID = "b1g88edpbhf5q2b4a37c"
+text = "привед, мир."
+
+
+def run(iam_token, folder_id, user_text):
+    # Собираем запрос
+    data = {}
+    # Указываем тип модели
+    data["modelUri"] = f"gpt://{folder_id}/yandexgpt/latest"
+    # Настраиваем опции
+    data["completionOptions"] = {"temperature": 0.3, "maxTokens": 1000}
+    # Указываем контекст для модели
+    data["messages"] = [
+        {
+            "role": "system",
+            "text": "Исправь ошибки в тексте."
+        },
+
+        {
+            "role": "user",
+            "text": f"{user_text}"
+        },
+    ]
+
+    # Отправляем запрос
+    response = requests.post(
+        URL,
+        headers={
+            "Accept": "application/json",
+            "Authorization": f"Bearer {iam_token}"
+        },
+        json=data,
+    ).json()
+
+    # Распечатываем результат
+    return response
+
+
+run_res = run(IAM_TOKEN, FOLDER_ID, text)
+gpt_response_text = run_res["result"]["alternatives"][0]["message"]["text"]
+print(gpt_response_text)
